268860bf4e001c30eb4238b3daadf82135173895
95tuanle <tlna95@icloud.com>
2022-10-26 03:46:43 -0400

    Add Activities

---
+++ app/src/main/java/humber/android/group/six/carshare/BookingHistoryActivity.java
@@ -0,0 +1,14 @@
+package humber.android.group.six.carshare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BookingHistoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_booking_history);
+    }
+}
