82bbbf5a9fb68595306258246a64ed692b301637
95tuanle <tlna95@icloud.com>
2022-11-15 02:00:04 -0500

    Clean up imports

--- app/src/main/java/humber/android/group/six/carshare/BookingHistoryAdapter.java
+++ app/src/main/java/humber/android/group/six/carshare/BookingHistoryAdapter.java
@@ -1,14 +1,12 @@
 package humber.android.group.six.carshare;
 
 import android.content.Context;
-import android.content.res.Resources;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import androidx.annotation.DrawableRes;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
